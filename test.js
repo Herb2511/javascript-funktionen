53fbd98502515286ce4835690a26cda1d94ae3b5
Herbert Haida <h.haida@digital-print-group.de>
2019-10-16 17:31:19 +0200

    New functions, ideas

--- test.js
+++ test.js
@@ -5,3 +5,35 @@ function doMath() {
     amount = (nValue * price);
     document.getElementById("total").value = amount;
 }
+
+var mb = [
+    "ASUS",
+    "Gigabyte",
+    "MSI",
+    "ASRock",
+];
+
+document.getElementById("demo").innerHTML = mb;
+
+
+
+Math.PI;
+
+a = 5;
+b = 6;
+c = a + b;
+
+console.log(mb + " Zahl: " + c);
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+
+}
