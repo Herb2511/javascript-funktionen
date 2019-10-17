549113986f3f0541b3ec89f391e3341f2458bea3
Herbert Haida <h.haida@digital-print-group.de>
2019-10-17 14:35:21 +0200

    Tests

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// Calculate input
 function doMath() {
     var nValue; var amount;
     var price = 100;
@@ -6,28 +7,28 @@ function doMath() {
     document.getElementById("total").value = amount;
 }
 
+// Array
 var mb = [
     "ASUS",
     "Gigabyte",
     "MSI",
     "ASRock",
+    "Biostar"
 ];
 
-document.getElementById("demo").innerHTML = mb;
+// Array output
+document.getElementById("array-output").innerHTML = mb[1];
 
-
-
+// Math and pi
 Math.PI;
 
+// Calculate and log on console
 a = 5;
 b = 6;
 c = a + b;
+console.log(mb + " Das Ergebnis ist: " + c);
 
-console.log(mb + " Zahl: " + c);
-
-
-
-
+// Show password checkbox
 function myFunction() {
     var x = document.getElementById("myInput");
     if (x.type === "password") {
@@ -35,5 +36,45 @@ function myFunction() {
     } else {
         x.type = "password";
     }
+}
+
+// Validate form
+function validateForm() {
+    var y = document.forms["myForm"]["fname"].value;
+    var b = document.getElementById("button1");
+    if (y == "") {
+        document.getElementById("hinweis").innerHTML = "Name muss ausgefüllt werden!";
+        b.style.display = "block";
+        return false;
+    }
+    if (y == "Herb") {
+        alert("Dein Name ist " + y + ".");
+    }
+}
+
+// Hide button after clicking "ok"
+function hideButton() {
+    var c = document.getElementById("button1");
+    c.style.display = "none";
 
 }
+
+// Constructor function for person objects
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+// Create a person object
+var myFather = new Person("Herbert", "Haida", 61, "grün");
+var myMother = new Person("Petra", "Haida", 59, "braun");
+var child = myFather.firstName + " " + myFather.lastName + ", " + myMother.eyeColor;
+var year = new Date().getUTCFullYear();
+var childage = year - 1985;
+
+// Display age
+document.getElementById("objekt").innerHTML = "Mein Kind, " + child + " ist " + childage + " Jahre alt.";
+
+console.log(child + " " + ", " + childage);
